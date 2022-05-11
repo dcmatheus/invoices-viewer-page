58299b8c4d618d9b542f65e796a7846d51815cd5
dcmatheus <mathws.costag@gmail.com>
2022-05-10 21:43:49 -0300

    update InvoicesListTable

--- src/templates/InvoicesList.vue
+++ src/templates/InvoicesList.vue
@@ -1,11 +1,11 @@
 <script setup>
 import InvoicesListTitle from '../components/InvoicesListTitle.vue';
-import InvoiceListTable from '../components/InvoiceListTable.vue';
+import InvoicesListTable from '../components/InvoicesListTable.vue';
 </script>
 
 <template>
   <div class="w-full py-12 mt-16 border-t-2 px-14 border-tertiary">
     <InvoicesListTitle />
-    <InvoiceListTable />
+    <InvoicesListTable />
   </div>
 </template>
