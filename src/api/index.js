e123096d13d0a727bc132aa8408182603d18ae87
dcmatheus <mathws.costag@gmail.com>
2022-05-13 06:45:51 -0300

    modify deployment system

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,6 @@ const url = import.meta.env.VITE_API_URL || 'http://localhost:3001/';
 
 async function getInvoices() {
   try {
-    console.log(url);
     const response = await fetch(`${url}orders`);
     const json = await response.json();
     return json;
